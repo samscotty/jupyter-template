05309ec53e54341c81cd1f0bdcf79ae4d7bf088b
Sam Scott <sam.scott7@me.com>
2023-01-07 17:56:20 +0000

    Posix shell compliant conditional

--- scripts/build_lib.sh
+++ scripts/build_lib.sh
@@ -7,7 +7,7 @@ set -eux
 
 sub_dir_count=$(find $1 -maxdepth 1 -type d | wc -l)
 
-if [[ "$sub_dir_count" -gt 1 ]]
+if [ "$sub_dir_count" -gt 1 ]
 then
     echo "Installing local Python packages"
     pip install --no-cache-dir $2
